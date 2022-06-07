c0c0b0b7a84a9c9a384f5e2e92d8dd4ed56a6c53
Maciek Kosinski <maciek.kosinski87@gmail.com>
2022-06-07 09:03:55 +0200

    nie dokończone

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -7,4 +7,30 @@ const reference = {
     galleryContainer: document.querySelector('.gallery'),
 };
 
-console.log(galleryItems);
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+reference.galleryContainer.innerHTML = galleryMarkup;
+
+const galleryModalWindow = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+});
+
+function createGalleryMarkup(galleryItems) {
+    return galleryItems.map(item => createGalleryItem(item)).join('');
+}
+
+function createGalleryItem({ preview, original, description }) {
+    return `
+      <a class="gallery__item" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          alt="${description}"
+          loading="lazy"
+        />
+      </a>`;
+}
+
+//console.log(galleryItems);
